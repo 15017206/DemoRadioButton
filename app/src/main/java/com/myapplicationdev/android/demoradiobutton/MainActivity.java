e68c3298136a87e2d91791c51241a5d93efa6e57
15017206 <15017206@myrp.edu.sg>
2017-04-20 14:21:23 +0800

    Updated

--- app/src/main/java/com/myapplicationdev/android/demoradiobutton/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoradiobutton/MainActivity.java
@@ -1,7 +1,13 @@
 package com.myapplicationdev.android.demoradiobutton;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.button);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+                int selectedBtnId = rg.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) findViewById(selectedBtnId);
+                Toast.makeText(getBaseContext(), rb.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
+
 }
